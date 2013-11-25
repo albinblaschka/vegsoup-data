53be076bea7e11c3ba19cf4fcbaf5274e794c828
kardinal-eros <roli@lore.fritz.box>
2013-11-25 16:12:35 +0100

    added MAKE file for barmstein dta

---
+++ barmstein dta/MakeVegsoup.R
@@ -0,0 +1,22 @@
+
+library(vegsoup)
+
+file <- "~/Documents/vegsoup-data/barmstein dta/species.csv"
+# promote to class "Species"
+
+X <- species(file, sep = ";")
+X <- X[, 1:4]
+
+file <- "~/Documents/vegsoup-data/barmstein dta/sites wide.csv"
+# promote to class "Sites"
+Y <- stack.sites(file = file)
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+# promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+# promote to class "Vegsoup"
+bs <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+save(bs, file = "~/Documents/vegsoup-data/barmstein dta/bs.rda")
+rm(list = ls()[-grep("bs", ls(), fixed = TRUE)])
+
