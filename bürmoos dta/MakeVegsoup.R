c9dfbf24505a17b2b8ff67533e246e48424cf3b7
kardinal-eros <roli@lore.fritz.box>
2013-11-25 16:20:09 +0100

    added MAKE file for bürmoos dta

---
+++ bürmoos dta/MakeVegsoup.R
@@ -0,0 +1,21 @@
+library(vegsoup)
+
+file <- "~/Documents/vegsoup-data/bürmoos dta/species.csv"
+# promote to class "Species"
+
+X <- species(file, sep = ";")
+X <- X[, 1:4]
+
+file <- "~/Documents/vegsoup-data/bürmoos dta/sites.csv"
+# promote to class "Sites"
+Y <- sites(read.csv2(file))
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+# promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+# promote to class "Vegsoup"
+bm <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+save(bm, file = "~/Documents/vegsoup-data/bürmoos dta/bm.rda")
+rm(list = ls()[-grep("bm", ls(), fixed = TRUE)])
+
