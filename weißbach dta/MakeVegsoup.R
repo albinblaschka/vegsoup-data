37ae1554c668d08ea9663002f9183fe8e2e92bcf
kardinal-eros <roli@lore.fritz.box>
2013-11-25 15:53:46 +0100

    added MAKE file for weißbach dta

---
+++ weißbach dta/MakeVegsoup.R
@@ -0,0 +1,24 @@
+library(vegsoup)
+
+file <- "~/Documents/vegsoup-data/weißbach dta/species.csv"
+# promote to class "Species"
+
+X <- species(file, sep = ";")
+X <- X[, 1:4]
+
+file <- "~/Documents/vegsoup-data/weißbach dta/sites wide.csv"
+
+# promote to class "Sites"
+Y <- stack.sites(file = file)
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+
+# promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(x = X, file.y = file)
+
+
+# promote to class "Vegsoup"
+wb <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+save(wb, file = "~/Documents/vegsoup-data/weißbach dta/wb.rda")
+#	QuickMap(wk)
