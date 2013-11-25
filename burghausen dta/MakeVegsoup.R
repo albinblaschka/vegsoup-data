c5a18b30e4b3c0963b44490f1063367c71096f27
kardinal-eros <roli@lore.fritz.box>
2013-11-25 16:17:47 +0100

    added MAKE file for burghausen dta

---
+++ burghausen dta/MakeVegsoup.R
@@ -0,0 +1,22 @@
+
+library(vegsoup)
+
+file <- "~/Documents/vegsoup-data/burghausen dta/species.csv"
+# promote to class "Species"
+
+X <- species(file, sep = ";")
+X <- X[, 1:4]
+
+file <- "~/Documents/vegsoup-data/burghausen dta/sites.csv"
+# promote to class "Sites"
+Y <- sites(read.csv2(file))
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+# promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+# promote to class "Vegsoup"
+bh <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+save(bh, file = "~/Documents/vegsoup-data/burghausen dta/ga.rda")
+rm(list = ls()[-grep("ba", ls(), fixed = TRUE)])
+
