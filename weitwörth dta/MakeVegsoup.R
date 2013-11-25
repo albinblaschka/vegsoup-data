07da95e0c32443db0f30937e763b9d496bfc789a
kardinal-eros <roli@lore.fritz.box>
2013-11-25 12:06:04 +0100

    added MAKE file to weitwörth dta

---
+++ weitwörth dta/MakeVegsoup.R
@@ -0,0 +1,21 @@
+library(vegsoup)
+
+file <- "~/Documents/vegsoup-data/weitwörth dta/species.csv"
+# promote to class "Species"
+
+X <- species(file, sep = ";")
+X <- X[, 1:4]
+file <- "~/Documents/vegsoup-data/weitwörth dta/sites wide.csv"
+
+# promote to class "Sites"
+Y <- stack.sites(file = file)
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+# promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+# promote to class "Vegsoup"
+ww <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+save(ww, file = "~/Documents/vegsoup-data/weitwörth dta/ww.rda")
+rm(X, Y, XZ, file)
+
