4f3fd88767d925eab7b722dde155a885fcbce032
kardinal-eros <roli@lore.fritz.box>
2013-11-25 22:48:16 +0100

    added meroth2013

---
+++ meroth2013/MakeVegsoup.R
@@ -0,0 +1,15 @@
+require(vegsoup)
+
+file <- "~/Documents/vegsoup-data/meroth2013/species wide.csv"
+X <- stack.species(file = file)[, 1:4]
+
+file <- "~/Documents/vegsoup-data/meroth2013/sites wide.csv"
+Y <- stack.sites(file = file)
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+XZ <- SpeciesTaxonomy(x = X, file.y = file)
+
+meroth2013 <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+save(meroth2013, file = "~/Documents/vegsoup-data/meroth2013/meroth2013.rda")
+rm(list = ls()[-grep("meroth2013", ls())])
