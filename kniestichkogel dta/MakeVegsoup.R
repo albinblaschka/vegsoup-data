2f6df80d2a674c623aab1f0358d80fafb6c4418f
kardinal-eros <roli@lore.fritz.box>
2013-11-25 14:35:40 +0100

    coded species and added MAKE file for kniestichkogl dta

---
+++ kniestichkogel dta/MakeVegsoup.R
@@ -0,0 +1,20 @@
+library(vegsoup)
+
+file <- "~/Documents/vegsoup-data/kniestichkogel dta/species.csv"
+# promote to class "Species"
+
+X <- species(file, sep = ";")
+X <- X[, 1:4]
+file <- "~/Documents/vegsoup-data/kniestichkogel dta/sites wide.csv"
+
+# promote to class "Sites"
+Y <- stack.sites(file = file)
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+# promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+# promote to class "Vegsoup"
+kn <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+save(kn, file = "~/Documents/vegsoup-data/kniestichkogel dta/kn.rda")
+rm(X, Y, XZ, file)
