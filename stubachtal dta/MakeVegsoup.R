b57393a5e71900590fda6e562dea7b404f25a138
kardinal-eros <roli@lore.fritz.box>
2013-12-14 14:21:01 +0100

    added szubachtal dta
    only 1 relevee

---
+++ stubachtal dta/MakeVegsoup.R
@@ -0,0 +1,21 @@
+library(vegsoup)
+
+file <- "~/Documents/vegsoup-data/stubachtal dta/species.csv"
+# promote to class "Species"
+
+X <- species(file, sep = ";")
+X <- X[, c(1:4, grep("voucher", names(X@data)))]
+file <- "~/Documents/vegsoup-data/stubachtal dta/sites wide.csv"
+
+# promote to class "Sites"
+Y <- stack.sites(file = file)
+
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+# promote to class "SpeciesTaxonomy"
+XZ <- SpeciesTaxonomy(X, file.y = file)
+# promote to class "Vegsoup"
+st <- Vegsoup(XZ, Y, coverscale = "braun.blanquet")
+
+save(st, file = "~/Documents/vegsoup-data/hagengebirge dta/st.rda")
+
+rm(X, Y, XZ, file)
