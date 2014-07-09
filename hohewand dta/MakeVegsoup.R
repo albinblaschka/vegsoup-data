efb350cef70b303cea6da553fe09d8dd80b6bfcc
Roland Kaiser <kardinal.eros@gmail.com>
2014-07-09 20:29:08 +0200

    added coordinates for 10 new plots …
    … and converted sites to wide format

--- hohewand dta/MakeVegsoup.R
+++ hohewand dta/MakeVegsoup.R
@@ -4,8 +4,8 @@ file <- "~/Documents/vegsoup-data/hohewand dta/species.csv"
 X <- species(file, sep = ";")
 X <- X[, c(1:4, grep("voucher", names(species(X))))]
 
-file <- "~/Documents/vegsoup-data/hohewand dta/sites.csv"
-Y <- sites(read.csv2(file))
+file <- "~/Documents/vegsoup-data/hohewand dta/sites wide.csv"
+Y <- stackSites(file = file, sep = ";")
 
 file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
 # promote to class "SpeciesTaxonomy"
