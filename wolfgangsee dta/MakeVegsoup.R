b371da217008f517d62ed7068c002044206a9c71
kardinal-eros <roli@Zora.local>
2014-11-11 22:50:30 +0100

    adapted Make file

--- wolfgangsee dta/MakeVegsoup.R
+++ wolfgangsee dta/MakeVegsoup.R
@@ -33,10 +33,11 @@ obj$richness <- richness(obj, "sample")
 do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
 write.verbatim(obj, file.path(path, "transcript.txt"), sep = "", add.lines = TRUE)
 
-#	decostand(obj) <- "pa"
-#	vegdist(obj) <- "bray"
-#	write.verbatim(seriation(obj), file.path(path, "seriation.txt"),
-#	sep = "", add.lines = TRUE)
-
+if (FALSE) {
+	decostand(obj) <- "pa"
+	vegdist(obj) <- "bray"
+	write.verbatim(seriation(obj), file.path(path, "seriation.txt"),
+	sep = "", add.lines = TRUE)
+}
 #	tidy up
 rm(list = ls()[-grep(key, ls())])
