728b236b4fb09704787b0c1f9bc5c82e0c323e50
Roland Kaiser <kardinal.eros@gmail.com>
2014-11-20 19:16:56 +0100

    translated data set

---
+++ wittmann2007/MakeVegsoup.R
@@ -0,0 +1,23 @@
+require(vegsoup)
+require(naturalsort)
+require(bibtex)
+
+path <- "~/Documents/vegsoup-data/wittmann2007"
+source(file.path(path, "MakeVegsoup1.R"))
+source(file.path(path, "MakeVegsoup2.R"))
+
+key <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8")$key
+
+#	bind data sets
+obj <- bind(Wittmann2007Tab1, Wittmann2007Tab2)
+
+#	assign result object
+assign(key, obj)
+
+do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
+
+#	save to disk
+write.verbatim(obj, file.path(path, "transcript2.txt"), sep = "", add.lines = TRUE)
+
+rm(list = ls()[-grep(key, ls())])
+rm(Wittmann2007Tab1, Wittmann2007Tab2)
---
+++ wittmann2007/MakeVegsoup1.R
@@ -0,0 +1,44 @@
+require(vegsoup)
+require(naturalsort)
+require(bibtex)
+require(stringr)
+
+path <- "~/Documents/vegsoup-data/wittmann2007"
+key <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8")$key
+tab <- "Tab1"
+key <- paste0(key, tab)
+
+#	taxon translation
+file <- file.path(path, "translate1.csv")
+A <- read.csv2(file, stringsAsFactors = FALSE)
+
+#	species
+file <- file.path(path, "Wittmann2007Tab1Appendix.csv")
+X <- read.csv2(file, stringsAsFactors = FALSE)
+names(X) <- gsub("X", "", names(X))
+
+#	join taxa translation with abbreviations
+stopifnot(identical(X$taxon, A$taxon))
+X <- cbind(A, X[, -1])
+
+#	build object of class "Species"
+X <- stackSpecies(x = X, schema = c("abbr", "layer", "taxon"))[, 1:4]
+
+#	sites
+file <- file.path(path, "Wittmann2007Tab1AppendixLocations.csv")
+
+#	build object of class "Sites"
+Y <- stackSites(file = file, sep = ";")
+
+#	build object of class "SpeciesTaxonomy", tests matching to reference list
+file <- "~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv"
+XZ <- SpeciesTaxonomy(x = X, file.y = file)
+
+#	build vegsoup object
+obj <- Vegsoup(XZ, Y, coverscale = "braun.blanquet2")
+
+#	assign rownames
+rownames(obj) <- paste(key, rownames(obj), sep = ":")
+
+#	assign result object
+assign(key, obj)
