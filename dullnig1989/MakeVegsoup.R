3dffc29bee73190faf64fc6ab0a89e1d626c112f
kardinal-eros <roli@Zora.fritz.box>
2014-12-09 14:24:45 +0100

    finished project

---
+++ dullnig1989/MakeVegsoup.R
@@ -0,0 +1,66 @@
+library(vegit)
+library(vegsoup)
+require(bibtex)
+
+path <- "~/Documents/vegsoup-data/dullnig1989"
+key <- read.bib(file.path(path, "references.bib"), encoding = "UTF-8")$key
+
+#	reference list
+Z <- read.csv2("~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv",
+	colClasses = "character")
+
+#	 ... and read authored translate list
+zz <- read.csv(file.path(path, "translate.csv"),
+	colClasses = "character")
+zz <- join(zz, Z)
+
+#	make function for tables 1:4
+make <- function (tab = 1) {
+	file <- file.path(path, paste0("Dullnig1989Tab", tab, ".txt"))
+	x <- read.verbatim(file, colnames = "Aufnahmenummer", layers = "@", vertical = FALSE)
+	x1 <- species(x)
+	
+	file <- file.path(path, paste0("Dullnig1989Tab", tab, "FooterSpecies.txt"))
+	x2 <- castFooter(file, first = FALSE, layers = "@")
+	x2 <- species(x2[, c(1,3,4,2)])
+	
+	X <- rbind(x1, x2)
+	X$cov[X$cov == "R"] <- "r"
+	species(X) <- zz
+	
+	y <- header(x)
+	names(y) <- c("pls", "expo", "slope", "pH", "dH", "hcov", "mcov")
+	y <- cbind(plot = as.character(rownames(y)), y)
+	Y <- stackSites(x = y, zeros = TRUE)
+	
+	r <- Vegsoup(X, Y, Z, coverscale = "braun.blanquet2")
+	return(r)
+}
+
+obj <- sapply(1:4, make)
+obj <- do.call("bind", obj)
+
+p <- readOGR("/Users/roli/Documents/vegsoup-data/dullnig1989", "Dullnig1989",
+	stringsAsFactors = FALSE)
+p <- p[match(p$PLOT, rownames(obj)), ]
+obj$longitude <- coordinates(p)[, 1]
+obj$latitude <- coordinates(p)[, 2]
+obj$accuracy <- p$ACCURACY
+
+coordinates(obj) <- ~longitude+latitude
+
+#	order layer
+Layers(obj)	 <- c("hl", "ml")
+
+#	assign result object
+assign(key, obj)
+
+#	richness
+obj$richness <- richness(obj, "sample")
+
+#	save to disk
+do.call("save", list(key, file = file.path(path, paste0(key, ".rda"))))
+write.verbatim(obj, file.path(path, "transcript.txt"), sep = "", add.lines = TRUE)
+
+#	tidy up
+rm(list = ls()[-grep(key, ls())])
--- dullnig1989/vegit.R
+++ dullnig1989/vegit.R
@@ -6,6 +6,15 @@ path <- "~/Documents/vegsoup-data/dullnig1989"
 file <- file.path(path, "Dullnig1989Tab1OCR.csv")
 csv2txt(file, 10, width = 5, vertical = FALSE, overwrite = F)
 
+#	format OCR table transcript for table 2
+file <- file.path(path, "Dullnig1989Tab2OCR.csv")
+csv2txt(file, 10, width = 5, vertical = FALSE, overwrite = F)
+
+#	format OCR table transcript for table 3
+file <- file.path(path, "Dullnig1989Tab3OCR.csv")
+csv2txt(file, 10, width = 5, vertical = FALSE, overwrite = F)
+
+
 #	format OCR table transcript for table 4
 file <- file.path(path, "Dullnig1989Tab4OCR.csv")
 m <- csv2txt(file, 10, width = 5, vertical = FALSE, overwrite = F)
