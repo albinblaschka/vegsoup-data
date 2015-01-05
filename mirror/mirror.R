9d223b8a6e14a919274b1704e14aa4c7e01833d6
Roland Kaiser <kardinal.eros@gmail.com>
2015-01-05 20:46:56 +0100

    update

--- mirror/mirror.R
+++ mirror/mirror.R
@@ -45,10 +45,10 @@ ii <- c(
 x <- x[-match(ii, x)]
 
 #	update
-sapply(file.path(path, x, "MakeVegsoup.R"), function (x) {
-	cat(x, "\n")
-	source(x)
-	})
+#sapply(file.path(path, x, "MakeVegsoup.R"), function (x) {
+#	cat(x, "\n")
+#	source(x)
+#	})
 
 #	biblographic entities	
 x <- sapply(file.path(path, x), function (x) {
@@ -85,6 +85,13 @@ l <- sapply(mget(k), function (x) compress(x, retain = c("author", "title", "acc
 X <- do.call("bind", l)
 
 save(X, file = file.path(path, "mirror", "mirror.rda"))
+
+#	literature data
+length(grep(":", rownames(X), fixed = TRUE))
+
+#	own data
+nrow(X) - length(grep(":", rownames(X), fixed = TRUE))
+
 #	test sites consitency
 #test <- X$accuracy
 #write.csv2(cbind(plot = rownames(X), test)[is.na(test), ],
