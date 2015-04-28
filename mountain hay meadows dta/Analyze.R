28deadbbc2901f13acb2b41ddab06785bc421ed5
kardinal-eros <roli@Zora.fritz.box>
2015-04-28 09:12:55 +0200

    update

--- mountain hay meadows dta/Analyze.R
+++ mountain hay meadows dta/Analyze.R
@@ -17,6 +17,15 @@ levels(x$orig_diag)[levels(x$orig_diag) == "Astrantio-Tristetum"] <- "Astrantio-
 levels(x$orig_diag)[levels(x$orig_diag) == "Poo-Tristetetum"] <- "Poo-Trisetetum"
 levels(x$orig_diag)[levels(x$orig_diag) == "Festuco-Cynocuretum"] <- "Festuco-Cynosuretum"
 
+#	climate and elevation
+r1 <- raster("~/Dropbox/öklim/dta/asc/00_Niederschlag-Jahr.asc")
+r2 <- raster("~/Dropbox/öklim/dta/asc/01_Temperatur-Jahr.asc")
+r3 <- raster("~/Google Drive/autdem/autdem.tif")
+
+x$prec <- extract(r1, x)
+x$temp <- extract(r2, x)
+x$elev <- extract(r3, spTransform(x, CRS("+init=epsg:31287")))
+
 p0 <- VegsoupPartition(x, clustering = "verband")
 as.table(p0, "verband")
 d0 <- as.dendrogram(p0, labels = "verband")
@@ -39,6 +48,8 @@ boxplot(t(apply(optimclass1(os), 2, rank, ties = "first")))
 p0 <- VegsoupPartition(x1, clustering = "verband")
 Latex(fidelity(p0, "TCR"), file = "./tex/selected alliances", template = TRUE, stat.min = 0.1, taxa.width = "70mm")
 
+Latex(fidelity(p0, "TCR"), file = "./tex/selected alliances summary", template = TRUE, stat.min = 0.1, taxa.width = "70mm", mode = 2)
+
 #	select candidate, fuzzy c-means
 p <- VegsoupPartition(x1, k = 7, method = "FCM")
 #	try to optimize
@@ -55,11 +66,19 @@ Latex(fidelity(p, "TCR"), file = "./tex/FCM TCR k7", template = TRUE, stat.min =
 Latex(fidelity(pp, "TCR"), file = "./tex/FCM TCR k7 optsil", template = TRUE, stat.min = 0.1, taxa.width = "70mm")
 
 #	reorder to dendrogram
-ppp <- p
-dp <- sapply(cut(as.dendrogram(ppp), 0)$lower, function (x) dendrapply(x, function (n) attr(n, "label")))
-k <- factor(partitioning(ppp))
-l <- cbind(k = levels(k), dp)
-levels(k) <- dp
-ppp@part <- as.numeric(as.character(k))
+ppp <- reorder(p, as.dendrogram(ppp))
 
 Latex(fidelity(ppp, "TCR"), file = "./tex/FCM TCR k7 order", template = TRUE, stat.min = 0.1, taxa.width = "70mm")
+
+Latex(fidelity(ppp, "TCR"), file = "./tex/FCM TCR k7 order", template = TRUE, stat.min = 0.1, taxa.width = "70mm")
+
+#	r <- raster("~/Google Drive/autdem/autdem.tif")
+
+
+table(ppp$prec < 1200, partitioning(ppp))
+
+pppp <- ppp[which(ppp$prec > 1200), ]
+par(mfrow =c(1,3))
+boxplot(prec ~ partitioning(ppp), data = sites(ppp))
+boxplot(temp ~ partitioning(ppp), data = sites(ppp))
+boxplot(elev ~ partitioning(ppp), data = sites(ppp))
